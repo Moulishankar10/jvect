9c10b2bb021ac04be7b40b702452ee87023d5d5d
ToastCoder <vicky.pcbasic@gmail.com>
2020-11-05 22:25:06 +0530

    Added angle()

--- src/pyvect.java
+++ src/pyvect.java
@@ -1,21 +1,31 @@
+import java.lang.Math;
+
 class pyvect
 {
     public static int dot(int arr1[], int arr2[])
     {
-        int dot_product = 0, n = 3;
+        int dot_prod = 0, n = 3;
         for(int i=0;i<n;i++)
         {
-            dot_product = dot_product + arr1[i]*arr2[i];
+            dot_prod = dot_prod + arr1[i]*arr2[i];
         }
-        return dot_product;
+        return dot_prod;
     }
 
     public static int[] cross(int arr1[],int arr2[])
     {
-        int cross_product[] = {0,0,0};
-        cross_product[0] = arr1[1]*arr2[2] - arr1[2]*arr2[1];
-        cross_product[1] = arr1[2]*arr1[0] - arr1[0]*arr2[2];
-        cross_product[2] = arr1[0]*arr2[1] - arr1[1]*arr2[0];
-        return cross_product;
+        int cross_prod[] = {0,0,0};
+        cross_prod[0] = arr1[1]*arr2[2] - arr1[2]*arr2[1];
+        cross_prod[1] = arr1[2]*arr1[0] - arr1[0]*arr2[2];
+        cross_prod[2] = arr1[0]*arr2[1] - arr1[1]*arr2[0];
+        return cross_prod;
+    }
+
+    public static double angle(int arr1[],int arr2[])
+    {
+        double mod_a = Math.sqrt((arr1[0]^2)+(arr1[1]^2)+(arr1[2]^2));
+        double mod_b = Math.sqrt((arr2[0]^2)+(arr2[1]^2)+(arr2[2]^2));
+        double ang = Math.acos((dot(arr1,arr2))/(mod_a*mod_b));
+        return ang;
     }
 }
