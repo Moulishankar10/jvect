6a3819d40276e3df92fd166857eeec6c04adac4d
Moulishankar10 <moulishankarmr@gmail.com>
2020-11-08 14:54:41 +0530

    Updated code

--- area.java
+++ area.java
@@ -4,32 +4,6 @@
 package pyvect.area;
 
 // IMPORTING REQUIRED LIBRARIES
-<<<<<<< HEAD
-import java.lang.*;
-import pyvect.pyvect;
-
-public class area 
-{
-    
-    public static double triangle_adj(int arr1[], int arr2[])
-    {
-        pyvect obj = new pyvect();
-        int c[] = obj.cross(arr1, arr2);
-        //for (int i=0;i<c.length;i++) System.out.println(i);    
-        double mod = obj.modVector(c);
-        //System.out.println(mod);    
-        double res = Math.sqrt(mod);
-        return res;
-    }
-    
-    //public static void main(String[] args) 
-    //{
-      //  int a[] = {1,2,3};
-       // int b[] = {4,5,6};
-        //double res = triangle_adj(a,b);
-        //System.out.println(res);    
-    //}
-=======
 import java.lang.Math;
 import pyvect.*;
 
@@ -51,5 +25,4 @@ public class area
         System.out.println(triangle_adj(a,b));
 
     }
->>>>>>> 25898c1dac1efbf45d0d1d1a5b2ecdc7ab791b09
 }
