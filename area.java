da77bdd93e151b086554e17d4c85483f02213482
ToastCoder <vicky.pcbasic@gmail.com>
2020-11-08 12:56:56 +0530

    Modified object type to static

--- area.java
+++ area.java
@@ -5,16 +5,24 @@ package pyvect.area;
 
 // IMPORTING REQUIRED LIBRARIES
 import java.lang.Math;
-import pyvect.pyvect;
+import pyvect.*;
 
 public class area 
 {
-    pyvect obj = new pyvect();
-    public static double triangle_adj(int arr1[], int arr2[])
-    {
-        int c[] = obj.cross(arr1, arr2);
-        double mod = obj.modVector(c);
+    static pyvect obj = new pyvect();
+
+    public static double triangle_adj(int arr1[], int arr2[]) {
+        int c[] = pyvect.cross(arr1, arr2);
+        double mod = pyvect.modVector(c);
         double res = Math.abs(Math.sqrt(mod));
         return res;
+    }
+
+    public static void main(String[]args)
+    {
+        int a[] = {1,2,3};
+        int b[] = {4,5,6};
+        System.out.println(triangle_adj(a,b));
+
     }
 }
