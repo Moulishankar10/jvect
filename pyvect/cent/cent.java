1548e77911bd55f58294b07907f966d7c3a6711d
Moulishankar10 <moulishankarmr@gmail.com>
2021-03-31 20:14:25 +0530

    Added centroid function for tetrahedron

--- pyvect/cent/cent.java
+++ pyvect/cent/cent.java
@@ -4,12 +4,15 @@ public class cent
 {
     public static double[] triangle(int[] arr1, int[] arr2,int[] arr3)
     {
-        double res[] = {0,0,0};
-        for (int i = 0; i<3; i++)
-        {
-            res[i] = arr1[i]+arr2[i]+arr3[i];
-            res[i] = res[i]*0.33;
-        }
+        double res[] = new double[3];
+        for (int i = 0; i<3; i++) res[i] = (arr1[i]+arr2[i]+arr3[i])*0.25;
+        return res;
+    }
+
+    public static double[] tetrahedron(int[] arr1, int[] arr2,int[] arr3, int[] arr4)
+    {
+        double res[] = new double[3];
+        for (int i=0; i<3; i++) res[i] = (arr1[i]+arr2[i]+arr3[i]+arr4[i])*0.25;
         return res;
     }
 } 
