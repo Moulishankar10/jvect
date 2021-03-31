5058c73515f357513e858b7d26434b28bdcfabac
Moulishankar10 <moulishankarmr@gmail.com>
2021-03-31 21:06:56 +0530

    Modified the code

--- pyvect/area/area.java
+++ pyvect/area/area.java
@@ -14,17 +14,14 @@ public class area
     private static int dot(int x[], int y[])
     {
         int dot_prod = 0;
-        for(int i=0;i<3;i++)
-        {
-            dot_prod = dot_prod + x[i]*y[i];
-        }
+        for(int i=0;i<3;i++) dot_prod = dot_prod + x[i]*y[i];
         return dot_prod;
     }
 
     // FUNCTION TO FIND THE CROSS PRODUCT
     private static int[] cross(int x[],int y[])
     {
-        int cross_prod[] = {0,0,0};
+        int cross_prod[] = new int[3];
         cross_prod[0] = x[1]*y[2] - x[2]*y[1];
         cross_prod[1] = x[2]*x[0] - x[0]*y[2];
         cross_prod[2] = x[0]*y[1] - x[1]*y[0];
@@ -55,12 +52,9 @@ public class area
         int c1[] = cross(arr1,arr2);
         int c2[] = cross(arr2,arr3);
         int c3[] = cross(arr3,arr1);
-        int c[] = {0,0,0};
+        int c[] = new int[3];
         
-        for (int i = 0;i<3;i++)
-        {
-            c[i] = c1[i]+c2[i]+c3[i];
-        }
+        for (int i = 0;i<3;i++) c[i] = c1[i]+c2[i]+c3[i];
 
         return Math.abs(0.5*modVector(c));
     }
