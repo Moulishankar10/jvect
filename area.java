1715d6817b0f7634c1e0c911e71d0ba22aad2008
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-08 13:59:46 +0530

    Updated the file as error free

--- area.java
+++ area.java
@@ -31,15 +31,12 @@ public class area
     public static double modVector(int arr[])
     {
         return Math.sqrt((arr[0]*arr[0])+(arr[1]*arr[1])+(arr[2]*arr[2]));
-    }
+    }   
 
     // FUNCTION TO FIND AREA OF TRIANGLE WHEN ADJACENT SIDES ARE GIVEN
     public static double triangle_adj(int arr1[], int arr2[]) 
     {
-        int c[] = cross(arr1, arr2);
-        double mod = modVector(c);
-        double res = Math.abs(Math.sqrt(mod));
-        return res;
+        return 0.5*modVector(cross(arr1, arr2));
     }
 
     // FUNCTION TO FIND AREA OF TRIANGLE WHEN POSITION VECTORS ARE GIVEN
@@ -49,33 +46,31 @@ public class area
         int c2[] = cross(arr2,arr3);
         int c3[] = cross(arr3,arr1);
         int c[] = new int[3];
-        
         for (int i = 0;i<3;i++) c[i] = c1[i]+c2[i]+c3[i];
-
-        return Math.abs(0.5*modVector(c));
+        return 0.5*modVector(c);
     }
 
     // FUNCTION TO FIND AREA OF QUADRILATERAL BASED ON DIAGONALS
     public static double quad(int arr1[],int arr2[])
     {
-        return Math.abs(0.5*modVector(cross(arr1,arr2)));
+        return 0.5*modVector(cross(arr1,arr2));
     }
 
     // FUNCTION TO FIND AREA OF PARALLELOGRAM WHEN TWO ADJACENT SIDES ARE GIVEN
     public static double parallelogram(int arr1[], int arr2[])
     {
-        return Math.abs(modVector(cross(arr1,arr2)));
+        return modVector(cross(arr1,arr2));
     }
 
     // FUNCTION TO FIND THE AREA OF TETRAHEDRON WHEN THREE POSITIONAL VECTORS ARE GIVEN
     public static double tetrahedron(int arr1[],int arr2[],int arr3[])
     {
-        return Math.abs(0.1666*dot(cross(arr1,arr2),arr3));
+        return 0.1666*dot(cross(arr1,arr2),arr3);
     }
-    
+    /*
     public static void main(String[] args){
-        int a[] = {-2,2,-3}, b[] = {3,4,-5};
-        System.out.println(parallelogram(a,b));
-        System.out.println(modVector(a));
+        int a[] = {1,2,3}, b[] = {3,4,-5}, c[] = {2,8,-7};
+        System.out.println(tetrahedron(a,b,c));
     }
+    */
 }
