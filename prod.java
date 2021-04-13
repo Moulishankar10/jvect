42045bb2f39e768cae0b182f0109480234bba590
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-13 20:17:37 +0530

    Updated prod.java

--- prod.java
+++ prod.java
@@ -5,6 +5,7 @@ package jvect.prod;
 
 // IMPORTING REQUIRED LIBRARIES
 import java.lang.Math;
+import java.util.*;
 
 public class prod
 {
@@ -32,16 +33,36 @@ public class prod
         return Math.sqrt((arr[0]*arr[0])+(arr[1]*arr[1])+(arr[2]*arr[2]));
     }
 
+    // FUNCTION TO FIND SCALAR TRIPLE PRODUCT FOR THE GIVEN THREE VECTORS
     public static int s3(int[] x, int[] y, int[] z)
     {
         return dot(cross(x,y),z);
     }
 
-    /*
+    // FUNCTION TO FIND SCALAR PRODUCT FOR THE GIVEN FOUR VECTORS
+    public static int s4(int[] w, int[] x, int[] y, int[] z)
+    {
+        return dot(cross(w,x),cross(y,z));
+    }
+
+    // FUNCTION TO FIND VECTOR TRIPLE PRODUCT FOR THE GIVEN THREE VECTORS
+    public static int[] v3(int[] x, int[] y, int[] z)
+    {
+        return cross(cross(x,y),z);
+    }
+
+    // FUNCTION TO FIND SCALAR PRODUCT FOR THE GIVEN FOUR VECTORS
+    public static int[] v4(int[] w, int[] x, int[] y, int[] z)
+    {
+        return cross(cross(w,x),cross(y,z));
+    }
+
+    
     // MAIN FUNCTION FOR TESTING PURPOSE
     public static void main(String[] args){
-        int a1[] = {1,2,3}, a2[] = {2,3,4}, a3[] = {4,2,8}, a4[] = {2,8,7};
-        System.out.println(s3(a1,a2,a3));
+        int a1[] = {1,2,3}, a2[] = {2,3,4}, a3[] = {4,2,8}, a4[] = {3,4,8};
+        int[] arr = v4(a1,a2,a3,a4);
+        System.out.println(Arrays.toString(arr));
     }
-    */
+    
 }
