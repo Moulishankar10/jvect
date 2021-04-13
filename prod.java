572370d14689096f01ec099f3b01908be283eb15
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-13 20:19:32 +0530

    Minor Changes

--- prod.java
+++ prod.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class prod
 {
     // FUNCTION TO FIND DOT PRODUCT
-    public static int dot(int x[], int y[])
+    public static int dot(int[] x, int[] y)
     {
         int dot_prod = 0;
         for(int i=0;i<3;i++) dot_prod += x[i]*y[i];
@@ -18,7 +18,7 @@ public class prod
     }
 
     // FUNCTION TO FIND CROSS PRODUCT
-    public static int[] cross(int x[],int y[])
+    public static int[] cross(int[] x,int[] y)
     {
         int[] cross_prod = new int[3];
         cross_prod[0] = x[1]*y[2] - x[2]*y[1];
@@ -28,7 +28,7 @@ public class prod
     }
 
     // FUNCTION TO FIND MODULUS OF A VECTOR
-    public static double modVector(int arr[])
+    public static double modVector(int[] arr)
     {
         return Math.sqrt((arr[0]*arr[0])+(arr[1]*arr[1])+(arr[2]*arr[2]));
     }
@@ -57,12 +57,12 @@ public class prod
         return cross(cross(w,x),cross(y,z));
     }
 
-    
+    /*
     // MAIN FUNCTION FOR TESTING PURPOSE
     public static void main(String[] args){
         int a1[] = {1,2,3}, a2[] = {2,3,4}, a3[] = {4,2,8}, a4[] = {3,4,8};
         int[] arr = v4(a1,a2,a3,a4);
         System.out.println(Arrays.toString(arr));
     }
-    
+    */
 }
